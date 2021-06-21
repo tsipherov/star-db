d708288b8c0b811807e101c842b8a0a6f0407dfc
tsipherov <tsipherov@gmail.com>
2021-06-21 22:08:00 +0300

    edit fetch api

--- src/index.js
+++ src/index.js
@@ -6,4 +6,6 @@ import "bootswatch/dist/darkly/bootstrap.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-fetch("https://swapi.dev/api/people/1/").then(response => response.json()).then(res => console.log(res))
+fetch("https://swapi.dev/api/people/1/")
+  .then((response) => response.json())
+  .then((res) => console.log(res));
